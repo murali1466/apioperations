bd0bc13802abb66a256f74e242698ab950a1b643
Murali <muralikrishnatangella1466@gmail.com>
2023-04-13 12:29:44 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [type, setType] = useState([]);
+  const [status, setStatus] = useState([]);
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    if (type !== null) {
+      setTitle(type.title);
+    }
+    console.log(type);
+  }, [type]);
+  function Get() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => setType(response.data));
+  }
+  function Put() {
+    axios
+      .put("https://jsonplaceholder.typicode.com/posts/1", {
+        title: "hello",
+      })
+      .then((response) => setType(response.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  function Post() {
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts/", {
+        title: "Hello World!",
+        body: "This is a new post.",
+      })
+      .then((response) => setType(response.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  function Delete() {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => {
+        setType(response.data);
+        setStatus(response.status);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(status);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={Get}>Get</button>
+      <button onClick={Put}>Put</button>
+      <button onClick={Post}>Post</button>
+      <button onClick={Delete}>Delete</button>
+      <h1>{title}</h1> 
+    </>
   );
-}
-
+};
 export default App;
